1b996ac27600d4acc56e5ed1809b157c0d258608
Yuna-Ko <178826@cloud.rangitoto.school.nz>
2025-08-15 11:03:11 +1200

    Testing out tk code

--- Python/hello.py
+++ Python/hello.py
@@ -1,5 +1,17 @@
 import tkinter as tk
 window = tk.Tk()  # Assign the main window to a variable
-greeting = tk.Label(text='test, test')  # Created the code to then add to window / widget
-greeting.pack()  # Makes the code visible
+label = tk.Label(
+    text="Hello, Tkinter",
+    foreground="white",  # Set the text color to white
+    background="black",  # Set the background color to black
+    width = 10,
+    height = 10
+)
+button = tk.Button(
+    text="Click Click",
+    width = 30
+)
+
+button.pack()
+label.pack()  # Makes the code visible
 window.mainloop()  # Starts the loop for main window
