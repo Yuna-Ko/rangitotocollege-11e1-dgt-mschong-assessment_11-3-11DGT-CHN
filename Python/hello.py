aa1ea546144b60f27e89351fa650c9ca8d089d27
Yuna-Ko <178826@cloud.rangitoto.school.nz>
2025-08-15 10:39:03 +1200

    Testing out the commit :D

--- Python/hello.py
+++ Python/hello.py
@@ -1,1 +1,5 @@
-print("Hello world!")
+import tkinter as tk
+window = tk.Tk()  # Assign the main window to a variable
+greeting = tk.Label(text='test, test')  # Created the code to then add to window / widget
+greeting.pack()  # Makes the code visible
+window.mainloop()  # Starts the loop for main window
