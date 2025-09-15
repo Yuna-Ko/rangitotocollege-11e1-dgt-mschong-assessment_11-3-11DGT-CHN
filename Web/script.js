cdf85e0a6c2781c6b430ae20eefe728853939df0
Yuna-Ko <178826@cloud.rangitoto.school.nz>
2025-09-15 13:34:13 +1200

    interation two of comp 1

--- Web/script.js
+++ Web/script.js
@@ -21,3 +21,17 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "inline-block";
 }
 
+var coll = document.getElementsByClassName("NAVCOLLASP");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
