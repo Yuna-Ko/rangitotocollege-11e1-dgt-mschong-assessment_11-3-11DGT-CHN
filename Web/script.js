0c800bdc5d54ea31a819f0c7ae1fd6e3c784de37
Yuna-Ko <178826@cloud.rangitoto.school.nz>
2025-10-05 00:07:49 +1300

    Added in images, finished inter 2 nav, started working on homepage ss format. Encounter issue, the divs are overlapping

--- Web/script.js
+++ Web/script.js
@@ -29,10 +29,17 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("CARD");
+  let ansslides = document.getElementsByClassName("ANSCARD");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "inline-block";
+  if (n > ansslides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = ansslides.length}
+  for (i = 0; i < ansslides.length; i++) {
+    ansslides[i].style.display = "none";
+  }
+  ansslides[slideIndex-1].style.display = "inline-block";
 }
